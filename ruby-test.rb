13b59f7a9db4a1e21b065e3f68b624dbd4eb5715
tekton <tekton@pyroturtle.com>
2012-05-07 13:40:19 -0700

    basic output working for directories of images

--- ruby-test.rb
+++ ruby-test.rb
@@ -1,41 +1,59 @@
 require 'rubygems'
 require 'exifr'
+require 'logger'
 
 def find_files(loc)
 	files = Dir.new(loc)
+	Dir.chdir(loc)
 	files.each do |file|
 		case file.downcase
 		when /.jpg\Z/
-			puts "A jpg!"
+			$log.debug "#{file} is a jpg!"
+			exif_data = nil
+			exif_data = EXIFR::JPEG.new(file)
+			exif_loop(exif_data)
 		end
 	end
 end
 
-img_file = ARGV.first
-exif_data = nil
-exif_data = EXIFR::JPEG.new(img_file)
-h = Hash.new()
-
-if exif_data.exif? then
-	i = exif_data.exif.to_hash
-	i.each_pair do |key,val|
-		#puts "#{key} :: #{val}"
-		#h[key]
-		if h.has_key?(key)
-			puts "#{key} exists!"
-			if h[key].has_key?(val)
-				h[key][val] += 1
+def exif_loop(exif_data)
+	if exif_data.exif? then
+		i = exif_data.exif.to_hash
+		i.each_pair do |key,val|
+			#puts "#{key} :: #{val}"
+			#h[key]
+			if $h.has_key?(key)
+				$log.debug "#{key} exists!"
+				if $h[key].has_key?(val)
+					$h[key][val] += 1
+				else
+					$h[key][val] = 1
+				end
 			else
-				h[key][val] = 1
+				$log.debug "Key didn't exit, making new hash of hash"
+				$h[key] = Hash.new()
+				$h[key][val] = 1
 			end
-		else
-			puts "Key didn't exit, making new hash of hash"
-			h[key] = Hash.new()
-			h[key][val] = 1
 		end
+	else
+		puts "NO DATA"
 	end
-else
-	puts "NO DATA"
 end
 
-puts h.inspect
+def i_h(h)
+	h.each_key do |k|
+		puts "#{k} ::"
+			h[k].each_pair do |key,val|
+				puts "     #{key} :: #{val}"
+			end
+	end
+end
+
+$h = Hash.new()
+$log = Logger.new('log')
+loc = ARGV.first
+find_files(loc)
+
+$log.debug $h.inspect
+
+i_h($h)

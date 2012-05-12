d9c2816a429b3c40f0d5eb62f9840eec5c6e4bfe
tekton <tyler@pyroturtle.com>
2012-05-12 12:31:20 -0700

    initial rails setup complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+H1R0::Application.config.secret_token = '1b62c4e6394ac406c1ffb9ff38ba35075f8f4d5c5838301ab1dc56c0e83ca9f40bd40f1be682eca15ae361a8890687633b4066e272e57616cc397cc1ffd3011b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+H1R0::Application.config.session_store :cookie_store, key: '_H1.R0_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# H1R0::Application.config.session_store :active_record_store

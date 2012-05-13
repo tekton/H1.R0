dc2ab0aec646be3b32b3297ec8566b05779674a4
tekton <tyler@pyroturtle.com>
2012-05-13 15:41:28 -0700

    incremental ui commit

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -5,9 +5,13 @@ class BrowseController < ApplicationController
     
     ### Get exif tags list w/count
     
-    @exif = ExifDatum.calculate(:count, :value, :group => "tag", :select => :tag)
+    #@exif = ExifDatum.calculate(:count, :value, :group => "tag", :select => :tag)
+    @exif = ExifDatum.select("tag, value, count(*) as count").group("tag, value")
     logger.info "######################"
-    logger.info @exif
+    #logger.info @exif
+    @exif.each do |exif_data|
+      logger.info exif_data.tag + " :: " + exif_data.value + " :: " + exif_data.count.to_s
+    end
     logger.info "######################"
   end
 end

32bdfc40506705ad459f735341bac447dc9c0d6a
tekton <tyler@pyroturtle.com>
2012-05-13 10:42:37 -0700

    incremental commit

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -2,5 +2,12 @@ class BrowseController < ApplicationController
   # GET /
   def index
     @images = Image.order(:name).limit(4)
+    
+    ### Get exif tags list w/count
+    
+    @exif = ExifDatum.calculate(:count, :value, :group => "tag", :select => :tag)
+    logger.info "######################"
+    logger.info @exif
+    logger.info "######################"
   end
 end

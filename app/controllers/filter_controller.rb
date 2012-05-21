8b97c184c3010ca21c0f5ce4f168d39fb443fdc7
tekton <tekton@pyroturtle.com>
2012-05-21 09:10:14 -0700

    getting first step towards filters

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -13,12 +13,22 @@ class FilterController < ApplicationController
       @val += "."+@format
     end
     
+    ### Get the data and modle it into various forms...
+    
+    @id_array = Array.new
+    
     @exif = ExifDatum.where("tag = ? AND value = ?", @filter, @val).joins(:image).includes(:image)
       @exif.each do |exif_datum|
         logger.info exif_datum.image_id
+        @id_array.push exif_datum.image_id
         logger.info exif_datum.image.location
       end
-    #@exi2 = ExifDatum.select("tag, value, count(*) as count").where("tag = ? AND value = ?", @filter, @val).group("tag, value").order("tag asc")
+      
+    @exi2 = ExifDatum.select("tag, value, count(*) as count").where("image_id IN (?)", @id_array).group("tag, value").order("tag asc")
+    #@exi2 = ExifDatum.all(:conditions => {["image_id = ?", @id_array]}) #.select("tag, value, count(*) as count") ##.group("tag, value").order("tag asc")
+      @exi2.each do |exi2_data|
+        logger.info exi2_data.tag + " :: " + exi2_data.count
+      end
     
   end
   
